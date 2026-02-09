df6bdaf9332d1d265784b32e7caad7be30e53739
syedakinzaali <syedakinzaali0925@gmail.com>
2026-02-09 16:51:34 +0500

    added cors fix

--- backend/main.py
+++ backend/main.py
@@ -30,6 +30,9 @@ from fastapi.middleware.cors import CORSMiddleware
 
 # FastAPI Application
 app = FastAPI()
+from fastapi.middleware.cors import CORSMiddleware
+
+app.add_middleware( CORSMiddleware, allow_origins=[""], allow_credentials=True, allow_methods=[""], allow_headers=["*"], )
 
 app.add_middleware(
     CORSMiddleware,
